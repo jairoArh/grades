455b85eba74e499d7a192a7108ed3a3bd95297d8
Jairo Armando <jairo.riano@uptc.edu.co>
2021-11-26 09:32:31 -0500

    Version 1.3 Logica y pruebas de la clase Student

--- src/logic/Student.java
+++ src/logic/Student.java
@@ -30,26 +30,53 @@ public class Student {
     }
 
     public void addInscription( Matter matter){
-
+        inscriptions.add( new Inscription( this, matter ) );
     }
 
     public ArrayList<Inscription> getInscriptions() {
         return (ArrayList<Inscription>) inscriptions.clone();
     }
 
-    public boolean addGrade(Matter matter, float grade, float percent ){
+    public boolean addGrade( Matter matter, float grade, float percent ){
+
+        for( Inscription inscription : inscriptions ){
+            if( inscription.getMatter() == matter ){
+                return inscription.addGrade( inscription.new Grade(grade,percent));
+            }
+        }
 
         return false;
     }
 
     public float calcDefinitive( Matter matter ){
 
+        for( Inscription inscription : inscriptions ){
+            if( inscription.getMatter() == matter ){
+                return inscription.calcDefinitive();
+            }
+        }
+
         return 0.0f;
     }
 
     public float calcAverage(){
 
-        return 0.0f;
+        float avg = 0.0f;
+
+        for( Inscription inscription : inscriptions ){
+            avg += inscription.calcDefinitive(); // avg = avg + inscription.calcDefinitive()
+        }
+
+        return avg / inscriptions.size();
+    }
+
+    public byte sumCredits(){
+        byte sum = 0;
+        for( Inscription inscription : inscriptions ){
+            sum += inscription.getMatter().getCredits();
+        }
+
+        return sum;
     }
 
     @Override
---
+++ test/source/logic/StudentTest.java
@@ -0,0 +1,96 @@
+package logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+    private Student lina;
+    private Matter calc1;
+    private Matter progra2;
+    private Matter bd;
+    private Matter net;
+
+    void setup(){
+        lina = new Student("201332464","Lina María Barrera");
+        calc1 = new Matter("Calculo I",(byte)4);
+        progra2 = new Matter("Programación II",(byte)4);
+        bd = new Matter("Bases de Dtos",(byte)3);
+        net = new Matter("Redes",(byte)3);
+
+        lina.addInscription(calc1);
+        lina.addInscription(progra2);
+        lina.addInscription(bd);
+        lina.addInscription(net);
+    }
+
+    void setupTwo(){
+        setup();
+        //calc1=3.5
+        lina.addGrade( calc1, 3.5f,60 );
+        lina.addGrade( calc1, 3.5f,40 );
+        lina.addGrade( calc1, 4.2f,0.5f );
+
+        //progra2 = 3.0
+        lina.addGrade( progra2,2.0f,25 ); //0.5
+        lina.addGrade( progra2,3.0f,30 ); //0.9
+        lina.addGrade( progra2,4.0f,25 ); //1.0
+        lina.addGrade( progra2,3.0f,20 ); //0.6
+
+        //bd 4.0
+        lina.addGrade( bd,4.0f, 100 ); //4.0
+
+        //net 2.7
+        lina.addGrade( net,1.5f,20); //0.3
+        lina.addGrade( net,2.0f,20); //0.4
+        lina.addGrade( net,3.5f,20); //0.7
+        lina.addGrade( net,2.5f,20); //0.5
+        lina.addGrade( net,4.0f,20); //0.8
+    }
+
+    @Test
+    void addGrade() {
+        //calc1=3.5
+        setup();
+        assertTrue( lina.addGrade( calc1, 3.5f,60 ) );
+        assertTrue( lina.addGrade( calc1, 3.5f,40 ) );
+        assertFalse( lina.addGrade( calc1, 4.2f,0.5f ) );
+
+        //progra2 = 3.0
+        assertTrue(lina.addGrade( progra2,2.0f,25 )); //0.5
+        assertTrue(lina.addGrade( progra2,3.0f,30 )); //0.9
+        assertTrue(lina.addGrade( progra2,4.0f,25 )); //1.0
+        assertTrue(lina.addGrade( progra2,3.0f,20 )); //0.6
+
+        assertTrue( lina.addGrade( bd,4.0f, 100 )); //4.0
+
+        //2.7
+        assertTrue( lina.addGrade( net,1.5f,20)); //0.3
+        assertTrue( lina.addGrade( net,2.0f,20)); //0.4
+        assertTrue( lina.addGrade( net,3.5f,20)); //0.7
+        assertTrue( lina.addGrade( net,2.5f,20)); //0.5
+        assertTrue( lina.addGrade( net,4.0f,20)); //0.8
+
+    }
+
+    @Test
+    void calcDefinitive() {
+        setupTwo();
+        assertEquals(3.5,lina.calcDefinitive( calc1 ) );
+        assertEquals(3.0,lina.calcDefinitive( progra2 ) );
+        assertEquals(4.0,lina.calcDefinitive( bd ) );
+        assertEquals(2.7,lina.calcDefinitive( net ),0.1 );
+    }
+
+    @Test
+    void calcAverage() {
+        setupTwo();
+        assertEquals(3.3,lina.calcAverage(  ),0.1 );
+    }
+
+    @Test
+    void sumCredits(){
+        setupTwo();
+        assertEquals(14,lina.sumCredits());
+    }
+}
